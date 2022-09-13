ce366a3a7d766c120863ace63adff884d8ce1237
Shanel Beebe <shanelbeebe@icloud.com>
2022-09-13 08:16:49 -0600

    Finished hangman

--- hangman/app.js
+++ hangman/app.js
@@ -3,6 +3,7 @@ const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p
 const board = document.querySelector('#letterBoard')
 const blanks = document.querySelector('.blanks')
 const hiddenLetters = document.querySelector('.hiddenLetters')
+const lives = document.querySelector('.lifeNum')
 
 // dynamically create letter buttons
 const makeBtns = (arr) => {
@@ -17,8 +18,9 @@ const makeBtns = (arr) => {
 }
 makeBtns(alphabet)
 // generate random word
-
+// random number between 3 & 12 for number of words from API
 const num = Math.floor((Math.random() * 9) + 3)
+let lifeCount
 
 fetch(`https://random-word-api.herokuapp.com/word?length=${num}`)
     .then(res => res.json())
@@ -26,7 +28,8 @@ fetch(`https://random-word-api.herokuapp.com/word?length=${num}`)
         const wordArray = data[0].split('')
         const word = data[0];
         console.log(word);
-        
+        lives.innerText = word.length
+        lifeCount = word.length
         wordArray.forEach(letter => {
             let blank = document.createElement('span')
             hidden = document.createElement('span')
@@ -48,14 +51,37 @@ fetch(`https://random-word-api.herokuapp.com/word?length=${num}`)
                     const matches = str.split('').filter(el => el === button.textContent)
                     items.forEach(item => {
                         if(item.textContent === matches[0]){
-
                          item.style.visibility = 'visible'
                         }
                     })
-
                 }
+                checkProgress(str, items)
+                lifeCount--
+                lives.innerText = lifeCount
             })
         })
     }
+
+    // Check if all letters have been guessed
+    const checkProgress = (str, items) => {
+        const itemsArr = Array.from(items)
+       const checkAnswer = itemsArr.filter(item => item.style.visibility === "visible")
+       if(checkAnswer.length === str.length){
+           Swal.fire('Great Job!', 'You guessed the word.', 'success')
+           setTimeout(function(){
+            location = ''
+          },2000)
+        }
+        
+        if(lifeCount === 1 && checkAnswer.length != str.length){
+            lives.innerText = 0
+            Swal.fire('Sorry!', `The answer was ${str.toUpperCase()}`, 'error')
+            setTimeout(function(){
+                location = ''
+              },3000)
+       }
+    }
+
+
 
 
